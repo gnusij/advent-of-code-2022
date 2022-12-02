1104f43fde8c8d64ed6b86afa627f3e0a3efe01b
gnusij <gnusij@duck.com>
2022-12-02 10:07:44 -0500

    golf 02

--- 02/main.py
+++ 02/main.py
@@ -37,7 +37,7 @@ def solution_a(data):
     #    elif LOSE[o] == m:
     #        return 0 + SHAPE[m]
     #print(sum([play(O[o], M[m]) for o, m in data]))
-    print(sum(["rrppssrpsr".count(O[o]+M[m])*3+(" "+S).find(M[m]) for o, m in data]))
+    print(sum(["rrppssrpsr".count(O[o]+M[m])*3+S.find(M[m])+1 for o, m in data]))
 
 def solution_b(data):
     #def play(o, m):
@@ -48,7 +48,7 @@ def solution_b(data):
     #    else: 
     #        return 6 + SHAPE[WIN[o]]
     #print(sum([play(O[o], m) for o, m in data]))
-    print(sum(["XYZ".find(m)*3+(" "+S).find(S[(S.find(O[o])+"XYZ".find(m)-1)%3]) for o, m in data]))
+    print(sum(["XYZ".find(m)*3+S.find(S[(S.find(O[o])+"XYZ".find(m)-1)%3])+1 for o, m in data]))
     
 
 def main():
