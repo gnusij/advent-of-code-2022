063da0abdee0374dabd3c0a8222410f4045d93d2
gnusij <gnusij@duck.com>
2022-12-02 09:49:51 -0500

    golf 02

--- 02/main.py
+++ 02/main.py
@@ -26,31 +26,32 @@ SHAPE = {
     's':3,
 }
 
+S = " rps"
+SEQ = "rps"
+COND = "rrppssrpsr"
+
 def solution_a(data):
-    def play(o, m):
-        if o == m:
-            return 3 + SHAPE[m]
-        elif WIN[o] == m: 
-            return 6 + SHAPE[m]
-        elif LOSE[o] == m:
-            return 0 + SHAPE[m]
-    score = 0
-    for o, m in data:
-        score += play(O[o], M[m])
-    print(score)
+    #def play(o, m):
+    #    if o == m:
+    #        return 3 + SHAPE[m]
+    #    elif WIN[o] == m: 
+    #        return 6 + SHAPE[m]
+    #    elif LOSE[o] == m:
+    #        return 0 + SHAPE[m]
+    #print(sum([play(O[o], M[m]) for o, m in data]))
+    print(sum(["rrppssrpsr".count(O[o]+M[m])*3+S.find(M[m]) for o, m in data]))
 
 def solution_b(data):
-    def play(o, m):
-        if m == "X": 
-            return 0 + SHAPE[LOSE[o]]
-        elif m == "Y": 
-            return 3 + SHAPE[o]
-        else: 
-            return 6 + SHAPE[WIN[o]]
-    score = 0
-    for o, m in data:
-        score += play(O[o], m)
-    print(score)
+    #def play(o, m):
+    #    if m == "X": 
+    #        return 0 + SHAPE[LOSE[o]]
+    #    elif m == "Y": 
+    #        return 3 + SHAPE[o]
+    #    else: 
+    #        return 6 + SHAPE[WIN[o]]
+    #print(sum([play(O[o], m) for o, m in data]))
+    print(sum(["XYZ".find(m)*3+S.find(SEQ[(SEQ.find(O[o])+"XYZ".find(m)-1)%3])  for o, m in data]))
+    
 
 def main():
     with open(Path(__file__).parent.joinpath('data.txt')) as f:
