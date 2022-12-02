c577d6f95a7fd67efa2bfdccba9e25bd1bd9776f
gnusij <gnusij@duck.com>
2022-12-02 00:50:53 -0500

    golf 01

--- 01/main.py
+++ 01/main.py
@@ -8,7 +8,8 @@ def solution_b(data):
 
 def main():
     with open(Path(__file__).parent.joinpath('data.txt')) as f:
-        data = list(map(lambda x: sum(map(int, x.split('\n'))), f.read().split('\n\n'))) 
+        data = [ sum(map(int, x.split('\n'))) for x in f.read().split('\n\n') ]
+        #data = list(map(lambda x: sum(map(int, x.split('\n'))), f.read().split('\n\n'))) 
     solution_a(data)
     solution_b(data)
 
