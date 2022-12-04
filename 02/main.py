aaa3a899380b0e67b787d01c016e5200a9f7d101
gnusij <gnusij@duck.com>
2022-12-04 02:51:14 -0500

    golf 02

--- 02/main.py
+++ 02/main.py
@@ -1,30 +1,24 @@
 
-O = {
-    'A':'r',
-    'B':'p',
-    'C':'s',
-}
-M = {
-    'X':'r',
-    'Y':'p',
-    'Z':'s',
-}
-S = "rps"
+def O(c):
+    return ord(c)-65
 
+def M(c):
+    return ord(c)-88
+
+def C(o,m):
+    return O(o), M(m)
 
 def solution_a(data):
-    print(sum(["rrppssrpsr".count(o+m)*3+S.find(m)+1 for o, m in data]))
-
+    print(sum(["0011220120".count(str(o)+str(m))*3+m+1 for o,m in data]))
 
 def solution_b(data):
-    print(sum([m*3+S.find(S[(S.find(o)+m-1)%3])+1 for o, m in data]))
+    print(sum([m*3+(o+m-1)%3+1 for o,m in data]))
     
-
 def main():
     import aoc
-    data = [x.split() for x in aoc.get(2022,2).splitlines()]
-    solution_a([[O[o],M[m]] for o,m in data])
-    solution_b([[O[o],"XYZ".find(m)] for o,m in data])
+    data = [C(*x.split()) for x in aoc.get(2022,2).splitlines()]
+    solution_a(data)
+    solution_b(data)
 
 
 if __name__ == "__main__":
