b288052b08f2f392fbbe5bc96286d6381a3bc4a5
gnusij <gnusij@duck.com>
2022-12-04 15:28:04 -0500

    golf 04

--- 04/main.py
+++ 04/main.py
@@ -1,19 +1,20 @@
 
-def e(l):
-    x = list(map(int,l.split('-')))
-    return set(range(x[0],x[1]+1))
+def T(d):
+    def e(l):
+        x = [*map(int,l.split('-'))]
+        return {*range(x[0],x[1]+1)}
+    return [[*map(e, l.split(','))] for l in d.splitlines()]
 
 def solution_a(data):
     def d(A,B):
         return A.issubset(B) or B.issubset(A)
-    print(sum([1 if d(*s) else 0 for s in data]))
+    print(sum([1 if d(*s) else 0 for s in T(data)]))
 
 def solution_b(data):
-    print(sum([1 if set.intersection(*s) else 0 for s in data]))
+    print(sum([1 if set.intersection(*s) else 0 for s in T(data)]))
 
 def main():
     from aoc import data
-    data = [[*map(e, l.split(','))] for l in data.splitlines()]
     solution_a(data)
     solution_b(data)
 
