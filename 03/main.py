36e914e941ed5e55ef4dd4bdbe91f86c0e572b09
gnusij <gnusij@duck.com>
2022-12-04 16:04:01 -0500

    golf 03

--- 03/main.py
+++ 03/main.py
@@ -1,26 +1,24 @@
 
-def s(l):
-    return set(list(l))
+def S(l):
+    return {*[*l]}
 
-def c(*s):
-    def p(c):
-        return ord(c)-96 if c.islower() else ord(c)-38
-    return sum([p(c) for c in set.intersection(*s)])
+def C(*s):
+    return sum([ord(c)-96 if c.islower() else ord(c)-38 for c in set.intersection(*s)])
 
-def solution_a(data):
-    def h(l):
+def q1(d):
+    def H(l):
         h = len(l)//2
         return [l[:h], l[h:]]
-    print(sum([c(*map(s, h(l))) for l in data]))
+    print(sum([C(*map(S, H(l))) for l in d]))
 
-def solution_b(data):
-    print(sum([c(*map(s, l)) for l in zip(*[iter(data)]*3)]))
+def q2(d):
+    print(sum([C(*map(S, l)) for l in zip(*[iter(d)]*3)]))
 
 def main():
     import aoc 
-    data = aoc.get(2022,3).splitlines()
-    solution_a(data)
-    solution_b(data)
+    d = aoc.get(2022,3).splitlines()
+    q1(d)
+    q2(d)
 
 if __name__ == "__main__":
     main()
