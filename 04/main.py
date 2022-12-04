93f1a5ff44fc704bce6fc9f534f86058c1a2a0a9
gnusij <gnusij@duck.com>
2022-12-04 00:48:59 -0500

    day 04

--- 04/main.py
+++ 04/main.py
@@ -1,30 +1,15 @@
 
 def ex(s):
     x = list(map(int,s.split('-')))
-    return set(range(x[0],x[1]+1))
+    return set(range(x[0], x[1]+1))
 
 def solution_a(data):
-    #p = 0
-    #for l in data:
-    #    A = ex(l[0])
-    #    B = ex(l[1])
-    #    if A.issubset(B) or B.issubset(A):
-    #        p +=1 
-    #print(p)
     def d(A,B):
-        if A.issubset(B) or B.issubset(A):
-            return 1
+        return A.issubset(B) or B.issubset(A)
     print(sum([1 if d(*map(ex, l)) else 0 for l in data]))
 
 
 def solution_b(data):
-    #o = 0
-    #for l in data:
-    #    A = ex(l[0])
-    #    B = ex(l[1])
-    #    if A.intersection(B):
-    #        o +=1 
-    #print(o)
     print(sum([1 if set.intersection(*map(ex, l)) else 0 for l in data]))
 
 def main():
