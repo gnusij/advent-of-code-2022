49c2e3049ae8a41b2277913eb985439bf834c1fb
gnusij <gnusij@duck.com>
2022-12-06 22:54:39 -0500

    golf 05

--- 05/main.py
+++ 05/main.py
@@ -1,76 +1,29 @@
-"""
-TODO: prolly cleaner to have boxes in reverse order
-"""
-    
 import re
 
 def T(d):
-    ins = [[*map(int, re.findall(r'\d+',s))] for s in d.split('\n\n')[1].splitlines()]
-    crt = [[s[x] for x in range(1,len(s),4)] for s in d.split('\n\n')[0].splitlines()[:-1]]
-    return ins, crt 
+    I = [[*map(int, re.findall(r'\d+',s))] for s in d[1].splitlines()]
+    B = [[s[x] for x in range(1,len(s),4)] for s in d[0].splitlines()]
+    B = [[x for x in b if x != ' '] for b in [*map(list, zip(*B[:-1][::-1]))]]
+    return I, B
 
-def print_crates(C):
-    " for debugging "
-    print()
-    [print(c) for c in C]
+def p(B):
+    print(''.join([b[-1] for b in B]))
 
-def find_top(c, n):
-    for i in range(len(c)): 
-        if c[i][n] != ' ':
-            return c[i][n]
+def q1(I, B):
+    for n, c1, c2 in I:
+        [B[c2-1].append(B[c1-1].pop()) for i in range(n)]
+    p(B)
 
-def find_top_ind(c, n):
-    for i in range(len(c)):
-        if c[i][n] != ' ':
-            return i
-    return len(c)
-
-def print_top_crate_letters(c):
-    print(''.join([find_top(c,i) for i in range(len(c[0]))]))
-
-def move_one(c, f, t):
-    dest_ind = find_top_ind(c, t)-1
-    if dest_ind < 0:
-        c.insert(0, [' ']*len(c[0]))
-        dest_ind = 0
-    orig_ind = find_top_ind(c, f)
-    c[dest_ind][t] = str(c[orig_ind][f])
-    c[orig_ind][f] = ' '
-    return c
-
-def move_n(c, n, f, t):
-    dest_ind = find_top_ind(c, t)-n
-    orig_ind = find_top_ind(c, f)
-    if dest_ind < 0:
-        for i in range(abs(dest_ind)):
-            c.insert(0, [' ']*len(c[0]))
-            dest_ind += 1
-            orig_ind += 1
-    
-    for i in range(n):
-        j = dest_ind+i
-        c[j][t] = str(c[orig_ind+i][f])
-        c[orig_ind+i][f] = ' '
-    return c
-
-def q1(d):
-    I, C = T(d)
-    for i in I:
-        for j in range(i[0]):
-            C = move_one(C, i[1]-1, i[2]-1)
-    print_top_crate_letters(C)
-
-def q2(d):
-    I, C = T(d)
-    for i in I:
-        C = move_n(C, i[0], i[1]-1, i[2]-1)
-    print_top_crate_letters(C)
+def q2(I, B):
+    for n, c1, c2 in I:
+        B[c2-1].extend([B[c1-1].pop() for i in range(n)][::-1])
+    p(B)
 
 def main():
     import aoc 
-    d = aoc.get(2022,5)
-    q1(d)
-    q2(d)
+    d = aoc.get(2022,5).split('\n\n')
+    q1(*T(d))
+    q2(*T(d))
 
 if __name__ == "__main__":
     main()
