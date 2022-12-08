cbf3e2df924007d1a522d7a1c42723d05d10fa8f
gnusij <gnusij@duck.com>
2022-12-07 23:58:02 -0500

    golf 07

--- 07/main.py
+++ 07/main.py
@@ -1,20 +1,18 @@
 import aoc 
 d=[d.split() for d in aoc.get(2022,7).split("$ ")[1:]][1:]
-o=p='/';F,D={},{}
-f=lambda p:p[:p[:-1].rfind('/')+1]
+o=p='/';F,D={},{};V='..'
+f=lambda p,v=V:p[:p[:-1].rfind('/')+1] if v==V else p+v+o
 for c in d:
- if c[1]=='..':p=f(p)
- elif c[0]=='cd':p+=c[1]+o
- elif c[0]=='ls':
+ if c[0]!='cd':
   while~-len(c):
-   *c,s,n=c
-   if s!='dir':F[p+n]=int(s)
+    *c,s,n=c
+    if s!='dir':F[p+n]=int(s)
+ else:p=f(p,c[1])
  D[p]=0
 for p in F:
- d=f(p)
- while d:
+ d=p
+ while d:=f(d):
   D[d]+=F[p]
   if d==o:break
-  d=f(d)
 print(sum([D[p] for p in D if D[p]<=100000]))
 print(min([D[p] for p in D if D[p]>=30000000-70000000+D[o]]))
