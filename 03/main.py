02d4ffeea8436c41cb511139d3f56c5bca50cb1a
gnusij <gnusij@duck.com>
2022-12-08 04:57:54 -0500

    golf 03

--- 03/main.py
+++ 03/main.py
@@ -1,25 +1,7 @@
-
-def S(l):
-    return {*[*l]}
-
-def C(*s):
-    return sum([ord(c)-96 if c.islower() else ord(c)-38 for c in set.intersection(*s)])
-
-def q1(d):
-    def H(l):
-        h = len(l)//2
-        return [l[:h], l[h:]]
-    print(sum([C(*map(S, H(l))) for l in d]))
-
-def q2(d):
-    print(sum([C(*map(S, l)) for l in zip(*[iter(d)]*3)]))
-
-def main():
-    import aoc 
-    d = aoc.get(2022,3).splitlines()
-    q1(d)
-    q2(d)
-
-if __name__ == "__main__":
-    main()
-
+import aoc 
+d=aoc.get(2022,3).splitlines()
+S=lambda l:{*[*l]}
+H=lambda l:[l[:len(l)//2],l[len(l)//2:]]
+C=lambda*s:sum([ord(c)-96 if c.islower() else ord(c)-38 for c in set.intersection(*s)])
+print(sum([C(*map(S,H(l))) for l in d]))
+print(sum([C(*map(S,l)) for l in zip(*[iter(d)]*3)]))
