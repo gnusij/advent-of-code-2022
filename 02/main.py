4a4842d185d0f35386e7fc0c0c727e282f546b8d
gnusij <gnusij@duck.com>
2022-12-08 17:56:23 -0500

    golf 02

--- 02/main.py
+++ 02/main.py
@@ -1,5 +1,4 @@
-import aoc
+import aoc;d=aoc.get(2022,2)
 C=lambda o,m:(ord(o)-65,ord(m)-88)
-d=[C(*x.split()) for x in aoc.get(2022,2).splitlines()]
-print(sum(["0011220120".count(str(o)+str(m))*3+m+1 for o,m in d]))
-print(sum([m*3+(o+m-1)%3+1 for o,m in d]))
+d=[C(*x.split()) for x in d.splitlines()]
+print(sum(["0011220120".count(str(o)+str(m))*3+m+1 for o,m in d]),sum([m*3+(o+m-1)%3+1 for o,m in d]))
