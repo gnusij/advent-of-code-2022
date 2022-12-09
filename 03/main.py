22f15b181a9fa88944998a05ec4239ff4fc64567
gnusij <gnusij@duck.com>
2022-12-09 04:45:11 -0500

    golf 03

--- 03/main.py
+++ 03/main.py
@@ -1,5 +1,5 @@
 import aoc;d=aoc.get(2022,3)
-d=d.splitlines()
+d=d.split()
 S=lambda l:{*[*l]}
 H=lambda l:[l[:len(l)//2],l[len(l)//2:]]
 C=lambda*s:sum([ord(c)-96 if c.islower() else ord(c)-38 for c in set.intersection(*s)])
