c6207e0358cf14854a7f01ecfaee517446576e02
gnusij <gnusij@duck.com>
2022-12-08 21:19:14 -0500

    golf 01

--- 01/main.py
+++ 01/main.py
@@ -1,4 +1,3 @@
-import aoc
-d=[sum(map(int,x.split())) for x in aoc.get(2022,1).split('\n\n')]
-print(max(d))
-print(sum(sorted(d)[::-1][:3]))
+import aoc;d=aoc.get(2022,1)
+d=[sum(map(int,x.split())) for x in d.split('\n\n')]
+print(max(d),sum(sorted(d)[::-1][:3]))
