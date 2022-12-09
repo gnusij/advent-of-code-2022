94eec22676ed1be5e633fdf07ff4284cac03dd25
gnusij <gnusij@duck.com>
2022-12-09 04:39:31 -0500

    golf 02

--- 02/main.py
+++ 02/main.py
@@ -1,8 +1,8 @@
 import aoc;d=aoc.get(2022,2)
 s=t=0
-for x in d.splitlines():
+for x in d.split('\n'):
  o,_,m=x
  o,m=ord(o)-65,ord(m)-88
- s+="0011220120".count(str(o)+str(m))*3+m+1
+ s+=int("210"[(o-m+1)%3])*3+m+1
  t+=m*3+(o+m-1)%3+1
 print(s,t)
