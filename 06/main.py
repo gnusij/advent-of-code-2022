b7c4b651c2c82a928124fb5883abbe9f574ed558
gnusij <gnusij@duck.com>
2022-12-09 04:39:41 -0500

    golf 06

--- 06/main.py
+++ 06/main.py
@@ -1,7 +1,7 @@
+import aoc;d=aoc.get(2022,6)
 def f(d,n): 
- i:=0 while not len({*d[i:i+n]})==n: i+=1
+ i=0
+ while not len({*d[i:i+n]})==n:i+=1
  print(i+n)
-import aoc 
-d=aoc.get(2022,6).strip()
 f(d,4)
 f(d,14)
