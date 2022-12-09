8243a6befd08606b4faa7d76b586fa7ab8fe5b94
gnusij <gnusij@duck.com>
2022-12-08 21:21:04 -0500

    golf 03

--- 03/main.py
+++ 03/main.py
@@ -1,7 +1,6 @@
-import aoc 
-d=aoc.get(2022,3).splitlines()
+import aoc;d=aoc.get(2022,3)
+d=d.splitlines()
 S=lambda l:{*[*l]}
 H=lambda l:[l[:len(l)//2],l[len(l)//2:]]
 C=lambda*s:sum([ord(c)-96 if c.islower() else ord(c)-38 for c in set.intersection(*s)])
-print(sum([C(*map(S,H(l))) for l in d]))
-print(sum([C(*map(S,l)) for l in zip(*[iter(d)]*3)]))
+print(sum([C(*map(S,H(l))) for l in d]),sum([C(*map(S,l)) for l in zip(*[iter(d)]*3)]))
