d68e30ce58565a60f0d84200833ab34a33213d6b
gnusij <gnusij@duck.com>
2022-12-09 23:53:49 -0500

    golf 02

--- 02/main.py
+++ 02/main.py
@@ -1,6 +1,5 @@
-import aoc;d=aoc.get(2022,2)
 s=t=0
-for x in d.split('\n'):
- o,_,m=x;o,m=ord(o)-65,ord(m)-88
+for o,_,m in open(0).read().split('\n'):
+ o,m=ord(o)-65,ord(m)-88
  s+=(m-o+1)%3*3+m+1;t+=m*3+(o+m-1)%3+1
 print(s,t)
