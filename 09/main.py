74f2be9dc56e985f2ecd43e0258a49908f3c6b7c
gnusij <gnusij@duck.com>
2022-12-11 06:13:04 -0500

    golf 09

--- 09/main.py
+++ 09/main.py
@@ -1,54 +1,19 @@
-from time import sleep
-import sys
-import os
-
 d = open(0).read()
-
-class Grid:
-    def __init__(self, l):
-        self.x=0 
-        self.y=0 
-        self.history=set()
-        self.chains=[(0,0)]*l
-        self.l=l
-        self.c=0
-
-    def do(self, inst):
-        j,n=inst.split()
-        for i in range(int(n)):
-            if j=="R": self.x+=1
-            elif j=="U": self.y+=1
-            elif j=="L": self.x-=1
-            elif j=="D": self.y-=1
-            
-            self.chains[0]=(self.x,self.y)
-
-            if self.c!=0:
-                for i in range(1,self.l):
-                    self.follow(i)
-
-            if self.chains[-1] not in self.history:
-                self.history.add(self.chains[-1])
-            self.c+=1
-
-    def follow(self, i):
-        a,b=self.chains[i-1]
-        A,B=self.chains[i]
-        dx = A-a
-        dy = B-b
-        adx = abs(dx)>1
-        ady = abs(dy)>1
-        X = [[-1,1][dx>1],0][not adx and ady]
-        Y = [[-1,1][dy>1],0][not ady and adx]
-        if adx or ady:
-            self.chains[i] = (a+X,b+Y)
-
-G=Grid(2)
-for inst in d.splitlines():
-    G.do(inst)
-print(len(G.history))
-
-G=Grid(10)
-for inst in d.splitlines():
-    G.do(inst)
-print(len(G.history))
+x=y=0
+t=0,0
+h,H,c,C={t},{t},[t]*2,[t]*10
+def m(c):
+ for i in range(1,len(c)):
+  a,b=c[i-1];A,B=c[i];d=A-a;D=B-b;u=abs(d)>1;v=abs(D)>1;c[i]=[A,[[a-1,a+1][d>1],a][~u+2 and v]][u or v],[B,[[b-1,b+1][D>1],b][~v+2 and u]][u or v]
+ return c
+for i in d.split('\n'):
+ j,n=i.split()
+ for k in range(int(n)):
+  if j=="R":x+=1
+  elif j=="U":y+=1
+  elif j=="L":x-=1
+  else:y-=1
+  c[0]=C[0]=x,y;c=m(c);C=m(C)
+  if{c[-1]}-h:h.add(c[-1])
+  if{C[-1]}-H:H.add(C[-1])
+print(len(h),len(H))
