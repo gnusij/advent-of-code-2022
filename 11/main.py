d4a9f6d39337e8f14625b7622e5a3249500a25ce
gnusij <gnusij@duck.com>
2022-12-11 02:17:01 -0500

    golf 11

--- 11/main.py
+++ 11/main.py
@@ -1,52 +1,22 @@
-class Monkey:
-    def __init__(self,mn):
-        self.mn = mn
-        self.items = []
-        self.op = ''
-        self.test = 0
-        self.tt = 0
-        self.tf = 0
-        self.i = 0
-
-d = open(0).read().split('\n')
-mn = -1
-monkeys = []
-while d:
-    l = d.pop(0)
-    if l.startswith("Monkey"):
-        monkeys.append(Monkey(mn))
-        mn+=1
-    if "Starting items" in l:
-        monkeys[mn].items = [*map(int, l.split(": ")[1].split(','))]
-    if "Operation" in l:
-        monkeys[mn].op = l.split(": ")[1].rstrip()
-    if "Test: "in l:
-        monkeys[mn].test = int(l.split(": ")[1].rstrip().split()[-1])
-    if "If true"in l:
-        monkeys[mn].tt = int(l.split(": ")[1].rstrip().split()[-1])
-    if "If false"in l:
-        monkeys[mn].tf = int(l.split(": ")[1].rstrip().split()[-1])
-
-super_modulo = 1
-for mon in monkeys:
-    super_modulo *= mon.test
-
-for r in range(1,10000+1):
-
-    for mon in monkeys:
-        while mon.items:
-            mon.i += 1
-            old = mon.items.pop(0)
-            exec(mon.op)
-            item = new % super_modulo
-            if item%mon.test == 0:
-                monkeys[mon.tt].items.append(item)
-            else:
-                monkeys[mon.tf].items.append(item)
-
-    if r in [1, 20, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]:
-        print(f"Round {r}")
-        print([mon.i for mon in monkeys])
-
-I = sorted([mon.i for mon in monkeys])[::-1]
-print(I[0]*I[1])
+n=0
+M=[]
+s=1
+f=10000
+for l in open(0).read().split('Monkey')[1:]:
+ l=l.split('\n')
+ S=lambda n:l[n].split(":")[1][1:]
+ N=lambda n:int(S(n).split()[-1])
+ M+=[0,[*map(int,S(1).split(','))],S(2),N(3),N(4),N(5)],
+ s*=N(3)
+ n+=1
+for _ in range(1,f+1):
+ for m in M:
+  while m[1]:
+   m[0]+=1
+   old=m[1].pop(0)
+   exec(m[2])
+   i=new%s
+   M[[m[5],m[4]][i%m[3]==0]][1]+=i,
+ if _ in [20,f]:
+  I=sorted([m[0] for m in M])[::-1]
+  print(I[0]*I[1])
