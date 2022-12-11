f2410acfc793e19935baaf2f50adea08924fece7
gnusij <gnusij@duck.com>
2022-12-11 01:46:32 -0500

    clean 11

--- 11/main.py
+++ 11/main.py
@@ -1,42 +1,31 @@
-from collections import deque
 class Monkey:
     def __init__(self,mn):
         self.mn = mn
-        self.items = deque()
+        self.items = []
         self.op = ''
-        self.opn = ''
         self.test = 0
-        self.testtrue = 0
-        self.testfalse = 0
+        self.tt = 0
+        self.tf = 0
         self.i = 0
 
-                
-
-
-#import aoc;d=aoc.get(2022,11)
-d=open(0).read()
-
-
-d = d.split('\n')
+d = open(0).read().split('\n')
 mn = -1
 monkeys = []
 while d:
-    line = d.pop(0)
-    #print(line)
-    if line.startswith("Monkey"):
-        mn+=1
+    l = d.pop(0)
+    if l.startswith("Monkey"):
         monkeys.append(Monkey(mn))
-    if "Starting items" in line:
-        monkeys[mn].items = deque([*map(int, line.split(": ")[1].split(','))])
-    if "Operation" in line:
-        monkeys[mn].op = line.split(": ")[1].rstrip()
-        monkeys[mn].opn = monkeys[mn].op.split()[-1]
-    if "Test: "in line:
-        monkeys[mn].test = int(line.split(": ")[1].rstrip().split()[-1])
-    if "If true"in line:
-        monkeys[mn].testtrue = int(line.split(": ")[1].rstrip().split()[-1])
-    if "If false"in line:
-        monkeys[mn].testfalse = int(line.split(": ")[1].rstrip().split()[-1])
+        mn+=1
+    if "Starting items" in l:
+        monkeys[mn].items = [*map(int, l.split(": ")[1].split(','))]
+    if "Operation" in l:
+        monkeys[mn].op = l.split(": ")[1].rstrip()
+    if "Test: "in l:
+        monkeys[mn].test = int(l.split(": ")[1].rstrip().split()[-1])
+    if "If true"in l:
+        monkeys[mn].tt = int(l.split(": ")[1].rstrip().split()[-1])
+    if "If false"in l:
+        monkeys[mn].tf = int(l.split(": ")[1].rstrip().split()[-1])
 
 super_modulo = 1
 for mon in monkeys:
@@ -47,13 +36,13 @@ for r in range(1,10000+1):
     for mon in monkeys:
         while mon.items:
             mon.i += 1
-            old = mon.items.popleft()
+            old = mon.items.pop(0)
             exec(mon.op)
             item = new % super_modulo
             if item%mon.test == 0:
-                monkeys[mon.testtrue].items.append(item)
+                monkeys[mon.tt].items.append(item)
             else:
-                monkeys[mon.testfalse].items.append(item)
+                monkeys[mon.tf].items.append(item)
 
     if r in [1, 20, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]:
         print(f"Round {r}")
