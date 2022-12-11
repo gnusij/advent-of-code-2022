dde53601bdf71f8017c0a1da2dfa98f1b05e0c0d
gnusij <gnusij@duck.com>
2022-12-10 23:53:43 -0500

    golf 10

--- 10/main.py
+++ 10/main.py
@@ -1,9 +1,8 @@
-import aoc;d=aoc.get(2022,10).rstrip()
 X=C=1
 S=P=0
 w,*Q=40,
 M=[[' ']*w for i in range(6)]
-for i in map(lambda x:x.split(),d.split('\n')):
+for i in map(lambda x:x.split(),open(0).read().split('\n')):
     Q+=[i,[0,1][len(i)-1]],
 while Q or P:
     if P==0:P=Q.pop(0)
@@ -11,7 +10,7 @@ while Q or P:
     if(C-20)%w==0:S+=C*X
     x=C%w-1
     if x in [X-1,X,X+1]:M[C//w][x]='#' 
-    P=[[i,c-1],0][c==0]
+    P=[0,[i,c-1]][c]
     if c<1and len(i)>1:X+=int(i[1])
     C+=1
 print(S)
